2416735bbf4917cfb8a5a959363ac9128a199cfe
N4EXM <naemmmail89@gmail.com>
2026-04-14 22:15:19 +0100

    addeding hyperlink functionality to the editor

---
+++ frontend/src/components/PostsPage/LinkModal.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import TextInput from '../inputs/TextInput'
+
+const LinkModal = ({ url, setUrl }) => {
+    return (
+        <div
+            className='bg-secondary rounded p-2'
+        >
+            <h2>
+                Link
+            </h2>
+            <div
+                className=''
+            >
+                <TextInput
+                    
+                />
+            </div>
+        </div>
+    )
+}
+
+export default LinkModal
--- frontend/src/components/inputs/RichTextInput.jsx
+++ frontend/src/components/inputs/RichTextInput.jsx
@@ -1,7 +1,10 @@
-import React, { useMemo } from "react";
-import { useEditor, Tiptap, EditorContext } from "@tiptap/react";
+import React, { useEffect, useMemo } from "react";
+import { useEditor, Tiptap, EditorContext, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from '@tiptap/extension-placeholder'
+import TextAlign from '@tiptap/extension-text-align'
+import Blockquote from "@tiptap/extension-blockquote";
+import Link from "@tiptap/extension-link";
 import Toolbar from "../btns/Toolbar";
 
 const RichTextInput = ({ content = `` }) => {
@@ -10,6 +13,20 @@ const RichTextInput = ({ content = `` }) => {
       StarterKit,
       Placeholder.configure({
         placeholder: 'Enter some content'
+      }),
+      TextAlign.configure({
+        types: ['heading', 'paragraph']
+      }),
+      Blockquote.configure({
+        HTMLAttributes: {
+          class: 'pl-4 max-w-none '
+        }
+      }),
+      Link.configure({
+        isAllowedUri: (url, ctx) => {
+          // Only allow HTTPS URLs
+          return ctx.defaultValidate(url) && url.startsWith('https://');
+        }
       })
     ],
     content: content,
@@ -17,21 +34,25 @@ const RichTextInput = ({ content = `` }) => {
       attributes: {
         // Add your Tailwind classes here
         class:
-          "prose prose-lg max-w-none focus:outline-none min-h-[200px] p-2 bg-background border-t-0 rounded-t-none border-2 rounded border-primary text-text placeholder:text-text/50",
+          "prose prose-lg max-w-none focus:outline-none min-h-[200px] p-2 bg-background border-t-0 rounded-t-none border-2 rounded border-primary/70 text-text placeholder:text-text/50",
       },
     },
     immediatelyRender: true
   });
 
-  const providerValue = useMemo(() => ({ editor }, [editor]))
+  const providerValue = useMemo(() => ({ editor }), [editor])
+
+  useEffect(() => {
+    console.log(editor)
+  }, [editor])
+
+  if (!editor) return null;
 
   return (
     <EditorContext.Provider value={providerValue}>
       <div className="flex flex-col gap-0 w-full h-full">
-        <Toolbar editor={editor}/>
-        <Tiptap editor={editor}>
-          <Tiptap.Content />
-        </Tiptap>
+        <Toolbar />
+        <EditorContent editor={editor}/>
       </div>
     </EditorContext.Provider>
   );
